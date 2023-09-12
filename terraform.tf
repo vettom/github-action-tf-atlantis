e0ae60e4be3f6c91b9ef2d0bd152c549764dc65f
Denny Vettom <dennysv@gmail.com>
2023-09-12 18:37:17 +0100

    Add terraform to init

---
+++ terraform.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket         = "github-action-aws-tf"
+    key            = "github-action-aws-tf/terraform.tfstate"
+    region         = "eu-west-1"
+    encrypt        = true
+    dynamodb_table = "github-action-aws-tf"
+    profile        = "default"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.13.1"
+    }
+  }
+}
+provider "aws" {
+  region = "eu-west-1"
+  profile = "default"
+}
